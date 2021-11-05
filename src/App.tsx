3184076faef00340dbae346b7c6aa33983c09f7d
Remtid <35769537+Remtid@users.noreply.github.com>
2021-11-06 00:35:50 +0100

    added Image component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import quillUIIcon from "./assets/icons/Quill.svg";
+import Image from './components/Image';
 
 function App() {
   return (
     <div className="App">
-      <img src={quillUIIcon} alt="QuillUI"/>
+      <Image source={quillUIIcon} alt="QuillUI" height="240px"/>
     </div>
   );
 }
